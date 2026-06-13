f1bf63c64ed3674d999c8c0d202d1921c8d85741
Anna Vorobey <annavorobey393@gmail.com>
2026-06-13 18:15:45 +0300

    Create session_loop.sh

---
+++ session_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SESSION="/var/tmp/session_timeout.txt"
+OUTPUT="/var/tmp/prompt_timeout.txt"
+
+# якщо файл порожній — 2 години
+if [[ ! -s "$SESSION" ]]; then
+    echo 7200 > "$SESSION"
+fi
+REM=$(<"$SESSION")
+echo  "rem" $(( REM ))
+while (( REM > 0 )); do
+        printf "Timeout: %02d:%02d:%02d\n" \
+            $((REM/3600)) $(((REM%3600)/60)) $((REM%60)) > "$OUTPUT"
+        REM=$(( REM - 1 ))
+        echo $(( REM )) > "$SESSION"
+    sleep 1
+done
+
+rm $SESSION
